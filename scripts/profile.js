c0c26550267b98b71294a33142956b0322b409a0
tommycoleman87 <tommiilee@gmail.com>
2019-06-25 16:20:47 -0400

    made the mobile bio function only activate at mobile viewport

--- scripts/profile.js
+++ scripts/profile.js
@@ -5,13 +5,16 @@ class Profile {
         this.bio = this.element.bio;
         this.bioInfo = document.querySelector(`.bio[data-bio='${this.data}']`);
         this.image = this.element.querySelector('img');
-        this.image.addEventListener('click', () => this.bioToggle());
+        this.image.addEventListener('click', () => {
+            if(parseInt(window.innerWidth) <= 500)
+            {this.bioToggleMobile()}});
     }
 
-    bioToggle() {
+    bioToggleMobile() {
         const bios = document.querySelectorAll('.bio');
         bios.forEach(para => para.classList.remove('bio-toggle'));
         this.bioInfo.classList.add('bio-toggle');
+        console.log(window.innerWidth);
     }
 }
 
