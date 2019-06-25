12f0422b8e82007579ff4df51037fd98511a9b40
tommycoleman87 <tommiilee@gmail.com>
2019-06-25 16:07:25 -0400

    implemented a clickable class to profile pictures to display a bio

---
+++ scripts/profile.js
@@ -0,0 +1,34 @@
+class Profile {
+    constructor(element){
+        this.element = element;
+        this.data = this.element.dataset.bio;
+        this.bio = this.element.bio;
+        this.bioInfo = document.querySelector(`.bio[data-bio='${this.data}']`);
+        this.image = this.element.querySelector('img');
+        this.image.addEventListener('click', () => this.bioToggle());
+    }
+
+    bioToggle() {
+        const bios = document.querySelectorAll('.bio');
+        bios.forEach(para => para.classList.remove('bio-toggle'));
+        this.bioInfo.classList.add('bio-toggle');
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const profiles = document.querySelectorAll('.profile');
+profiles.forEach(profile => new Profile(profile));
