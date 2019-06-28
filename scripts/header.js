162079be8386082fb6503d42a21f6e795ecc1478
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-06-28 15:50:45 -0400

    made rateLimitedFunctionFactory

--- scripts/header.js
+++ scripts/header.js
@@ -1,37 +1,45 @@
 (() => {
+  function rateLimitedFunctionFactory({ sideEffect, timeout = 300 }) {
+    let locked = false;
+    let firedWhileLocked = false;
+
+    return () => {
+      if (!locked) {
+        locked = true;
+        sideEffect();
+        setTimeout(() => {
+          locked = false;
+          if (firedWhileLocked) {
+            firedWhileLocked = false;
+            sideEffect();
+          }
+        }, timeout);
+      } else {
+        firedWhileLocked = true;
+      }
+    };
+  }
+
   function newMenuButton({ menuBtnElement }) {
     const _nav = document.getElementsByTagName('nav')[0];
-    const _timeout = 300;
     const _maxMobileWidth = 500;
 
     const toggleNav = () => {
       _nav.classList.toggle('menu--open');
     };
 
-    (() => {
-      let locked = false;
-      let firedWhileLocked = false;
 
-      window.addEventListener('resize', () => {
-        if (!locked) {
-          locked = true;
-          if (parseInt(window.innerWidth, 10) > _maxMobileWidth) {
-            _nav.classList.remove('menu--open');
-          }
-          setTimeout(() => {
-            locked = false;
-            if (firedWhileLocked && parseInt(window.innerWidth, 10) > _maxMobileWidth) {
-              firedWhileLocked = false;
-              _nav.classList.remove('menu--open');
-            }
-          }, _timeout);
-        } else {
-          firedWhileLocked = true;
+    const conditionallyCloseMenu = rateLimitedFunctionFactory({
+      sideEffect: () => {
+        if (parseInt(window.innerWidth, 10) > _maxMobileWidth) {
+          _nav.classList.remove('menu--open');
         }
-      });
-    })();
+      },
+      // timeout: 250, this line is where you can optionally change the default timeout duration
+    });
 
     menuBtnElement.addEventListener('click', toggleNav);
+    window.addEventListener('resize', conditionallyCloseMenu);
   }
 
   newMenuButton({ menuBtnElement: document.querySelector('.menu-btn') });
