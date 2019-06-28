e8104866289b506e94df7c8f1c4b82ce438216bf
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-06-28 13:12:12 -0400

    added logic to fire resize event one last time if the event it was triggered while locked

--- scripts/header.js
+++ scripts/header.js
@@ -3,26 +3,35 @@
     const _nav = document.getElementsByTagName('nav')[0];
     const _timeout = 300;
     const _maxMobileWidth = 500;
-    let _locked = false;
 
     const toggleNav = () => {
       _nav.classList.toggle('menu--open');
     };
 
-    const closeNavIfNotInMobileView = () => {
-      if (!_locked) {
-        _locked = true;
-        if (parseInt(window.innerWidth, 10) > _maxMobileWidth) {
-          _nav.classList.remove('menu--open');
+    (() => {
+      let locked = false;
+      let firedWhileLocked = false;
+
+      window.addEventListener('resize', () => {
+        if (!locked) {
+          locked = true;
+          if (parseInt(window.innerWidth, 10) > _maxMobileWidth) {
+            _nav.classList.remove('menu--open');
+          }
+          setTimeout(() => {
+            locked = false;
+            if (firedWhileLocked && parseInt(window.innerWidth, 10) > _maxMobileWidth) {
+              firedWhileLocked = false;
+              _nav.classList.remove('menu--open');
+            }
+          }, _timeout);
+        } else {
+          firedWhileLocked = true;
         }
-        setTimeout(() => {
-          _locked = false;
-        }, _timeout);
-      }
-    };
+      });
+    })();
 
     menuBtnElement.addEventListener('click', toggleNav);
-    window.addEventListener('resize', closeNavIfNotInMobileView);
   }
 
   newMenuButton({ menuBtnElement: document.querySelector('.menu-btn') });
